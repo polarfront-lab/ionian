0dea06ce64e876ae55ae38577f63c50c31cf0347
state303 <state303@dsub.io>
2025-02-23 16:14:43 +0900

    ci: audit no longer checks dev-deps

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig(({ command, mode }): UserConfig => {
         output: {
           globals: {
             three: 'THREE',
+            'three-stdlib': 'three-stdlib',
           },
         },
       },
